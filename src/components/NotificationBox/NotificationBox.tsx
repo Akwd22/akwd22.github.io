a3e72c5346f9c88ed7cd8554a533b9e592f28b28
Akwd22 <60796682+Akwd22@users.noreply.github.com>
2022-10-08 19:50:16 +0200

    Add: notifications are automatically closed after few seconds
    ... unless hovered

--- src/components/NotificationBox/NotificationBox.tsx
+++ src/components/NotificationBox/NotificationBox.tsx
@@ -1,5 +1,5 @@
 import { NotificationData } from "notification";
-import { FunctionComponent, useEffect } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 
 import ButtonIcon from "components/ButtonIcon/ButtonIcon";
@@ -13,11 +13,28 @@ interface NotificationBoxProps extends NotificationData {
 }
 
 /** Composant d'une notification. */
-const NotificationBox: FunctionComponent<NotificationBoxProps> = (props) => {
+const NotificationBox: FunctionComponent<NotificationBoxProps> = ({ text, onClose }) => {
+  /** Notification en train d'être fermée ? */
+  const [closing, setClosing] = useState(false);
+  /** Notification est-elle survolée par la souris ? */
+  const [hovered, setHovered] = useState(false);
+
+  // Fermer la notification si elle vient de l'être.
+  useEffect(() => {
+    if (closing) onClose();
+  }, [closing, onClose]);
+
+  // Fermer automatiquement la notification au bout de quelques secondes,
+  // si elle n'est pas survolée.
+  useEffect(() => {
+    const closeTimeout = !hovered ? setTimeout(() => setClosing(true), 3000) : null;
+    return () => clearTimeout(closeTimeout);
+  }, [hovered]);
+
   return createPortal(
-    <div className="notification">
-      <span className="notification-text">{props.text}</span>
-      <ButtonIcon className="notification-close" icon={<CrossIcon />} tooltip="Fermer la notification" onClick={props.onClose} />
+    <div className="notification" onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+      <span className="notification-text">{text}</span>
+      <ButtonIcon className="notification-close" icon={<CrossIcon />} tooltip="Fermer la notification" onClick={() => setClosing(true)} />
     </div>,
     document.getElementById("notification")
   );
